fa62f19f8cc24041d764c6819f49092e4c1e3cf2
Thomas Aunvik <thaun1@live.no>
2018-12-24 23:01:55 +0100

    Added Custom Object, testing

---
+++ workers/unity/Assets/Scripts/EntityCreationBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using Improbable;
+using Improbable.Gdk.Core;
+using Improbable.Gdk.Core.Commands;
+using Improbable.Gdk.GameObjectRepresentation;
+using Improbable.Worker.CInterop;
+using UnityEngine;
+
+namespace GdkTest
+{
+    public class EntityCreationBehaviour : MonoBehaviour
+    {
+        [Require] private WorldCommands.Requirable.WorldCommandRequestSender commandSender;
+        [Require] private WorldCommands.Requirable.WorldCommandResponseHandler responseHandler;
+
+        private void OnEnable()
+        {
+            responseHandler.OnCreateEntityResponse += OnCreateEntityResponse;
+        }
+
+        [ContextMenu("Test Create Entity")]
+        public void CreateEntity(Coordinates coords)
+        {
+            EntityTemplate template = ObjectTemplate.CreateObjectEntityTemplate(coords);
+            commandSender.CreateEntity(template);
+        }
+
+        void OnCreateEntityResponse(WorldCommands.CreateEntity.ReceivedResponse response)
+        {
+            if (response.StatusCode == StatusCode.Success)
+            {
+                var createdEntityId = response.EntityId.Value;
+                if(response.Context is Generated.Object)
+                {
+                    Generated.Object obj = response.Context as Generated.Object;
+                    Debug.Log("Created Entity Object...");
+                }
+                Debug.Log("Recieved Object");
+            }
+            else
+            {
+                // handle failure
+                Debug.LogError("Failed Recieving Object.\n" + response.Message);
+            }
+        }
+    }
+}
---
+++ workers/unity/Assets/Scripts/ObjectTemplate.cs
@@ -0,0 +1,35 @@
+﻿using Improbable;
+using Improbable.Gdk.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GdkTest.Generated;
+
+namespace GdkTest
+{
+    public static class ObjectTemplate
+    {
+        public static readonly List<string> AllWorkerAttributes = new List<string>()
+        {
+            "UnityClient",
+            "UnityGameLogic"
+        };
+
+        public static EntityTemplate CreateObjectEntityTemplate(Coordinates coords)
+        {
+            var objectComponent = GdkTest.Generated.Object.Component.CreateSchemaComponentData(message: "test");
+
+            var entity = EntityBuilder.Begin()
+                .AddPosition(coords.X, coords.Y, coords.Z, "UnityGameLogic")
+                .AddMetadata("Object", "UnityGameLogic")
+                .SetPersistence(true)
+                .SetReadAcl(AllWorkerAttributes)
+                .AddComponent(objectComponent, "UnityGameLogic")
+                .Build();
+
+            return entity;
+        }
+    }
+}
